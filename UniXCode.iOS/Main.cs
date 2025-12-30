633269b9664826953a6b9b7bca79518a4efe1138
ModxVoldHunter <modxvoldhunter@gmail.com>
2025-12-30 10:19:34 -0500

    Made Changes to Developmet Approach

---
+++ UniXCode.iOS/Main.cs
@@ -0,0 +1,24 @@
+using UIKit;
+using System;
+using Microsoft.VisualBasic;
+using Avalonia.Controls;
+using ARKit;
+
+namespace UniXCode.iOS;
+
+public class Application
+{
+    // This is the main entry point of the application.
+    static void Main(string[] args)
+    {
+        // if you want to use a different Application Delegate class from "AppDelegate"
+        // you can specify it here.
+        UIApplication.Main(args, null, typeof(AppDelegate));
+    }
+
+    public static void Button_OnClick(object sender, EventArgs args)
+    {
+
+        
+    }
+}
---
+++ UniXCode/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace UniXCode.ViewModels;
+
+public abstract class ViewModelBase : ObservableObject
+{
+}
